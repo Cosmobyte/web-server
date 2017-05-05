49f59ede7d77fc35a1542c9625eca27d1cfd8958
Cosmin-Mirel Budeanu <cosmin.mirel18@gmail.com>
2017-05-05 12:22:55 +0200

    Refactor middleware

---
+++ middleware.js
@@ -0,0 +1,14 @@
+
+	var middleware = {
+	requireAuthetification: function(req,res,next){
+		console.log('private route hit!');
+		next();
+	},
+	logger : function(req,res,next){
+		var date = new Date();
+		console.log('Request: '+ date + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+module.exports = middleware;
--- server.js
+++ server.js
@@ -2,17 +2,9 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
-var middleware = {
-	requireAuthetification: function(req,res,next){
-		console.log('private route hit!');
-		next();
-	},
-	logger : function(req,res,next){
-		var date = new Date();
-		console.log('Request: '+ date + req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
+var middleware = require('./middleware.js');
+
+
 app.use(middleware.logger);
 // app.use(middleware.requireAuthetification);
 
